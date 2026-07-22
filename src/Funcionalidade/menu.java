40e637bac3db9307d5e7fff06713353117e68547
athoscannavo <athoscannavo@gmail.com>
2026-07-22 14:20:08 -0300

    Athos: Adicionei a funcionalidade menu e fiz o chamamento dela no Main.java. deixei um espaço reservado na Funcionalidade menu para colocar o método analisar.

---
+++ src/Funcionalidade/menu.java
@@ -0,0 +1,50 @@
+package Funcionalidade;
+
+package Funcionalidade;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    // Método para exibir o menu e receber a opção do usuário
+    public static void ExibirMenu() {
+        Scanner leia = new Scanner(System.in);
+        int opcao = -1;
+
+        while (opcao != 2) {
+            System.out.println("\n=== Verificador de E-mail ===");
+            System.out.println("1. Inserir um e-mail para ser analisado");
+            System.out.println("2. Encerrar Programa");
+            System.out.print("Opção: ");
+
+            // Valida se foi digitado um número inteiro
+            if (leia.hasNextInt()) {
+                opcao = leia.nextInt();
+                leia.nextLine(); // Limpa o "Enter" pendente
+            } else {
+                System.out.println("\nOpção Inválida. Insira apenas números inteiros.");
+                leia.nextLine(); // Descarta a entrada inválida
+                continue;
+            }
+
+            // Direciona a ação da opção
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o texto a ser analisado: ");
+                    String textoEmAnalise = leia.nextLine();
+                    System.out.println("\nTexto recebido com sucesso.");
+                    System.out.println("Texto Recebido: " + textoEmAnalise);
+                    break;
+
+                case 2:
+                    System.out.println("\nEncerrando o programa...");
+                    break;
+
+                default:
+                    System.out.println("\nOpção Inválida. Escolha 1 ou 2.");
+                    break;
+            }
+        }
+        leia.close();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Funcionalidade.Menu;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        // Executa o menu interativo
+        Menu.ExibirMenu();
     }
 }
