facff533f5e1a1ebec163dd04e56d3756e97453d
joaoPdevs <joaoritter2006@gmail.com>
2026-07-22 14:17:51 -0300

    Fix(Corrigir o codigo para teste)

--- src/TesteTeste.java
+++ src/TesteTeste.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import Funcionalidade.analisar;
 
-public class https://medium.com/@habbema/java-hello-world-6dc92ce90a63TesteTeste {
+public class TesteTeste{
     public static void main(String[] args){
         Scanner leia = new Scanner(System.in);
         System.out.println("Digite o conteudo do Email");
