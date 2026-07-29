83d759b4efe08b7137e2be55c7f27158dbcba321
athoscannavo <athoscannavo@gmail.com>
2026-07-29 16:38:53 -0300

    Exibir Grau agora exibe o valor correto para grau.

--- src/Funcionalidade/ExibirGrau.java
+++ src/Funcionalidade/ExibirGrau.java
@@ -2,15 +2,15 @@ package Funcionalidade;
 
 public class ExibirGrau {
     public static String DefinirGrau(int score) {
-        String grau = "";
+        String grau;
 
         if(score >= 0 && score <= 30) {
             grau = "Provavelmente Legítima";
         }
-        if(score > 30 && score < 100 ) {
+        else if(score > 30 && score < 100 ) {
             grau = "Suspeita";
         }
-        if(score >= 100) {
+        else if(score >= 100) {
             grau = "Provavelmente é Golpe";
         }
         else {
